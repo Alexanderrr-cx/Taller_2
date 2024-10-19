416b88b023a4c5fe0dc3f3a7a1ce084d21eaca86
Alexanderrr-cx <165387853+Alexanderrr-cx@users.noreply.github.com>
2024-10-19 13:56:44 -0500

    tercer commit, trabajando con Git y Githubclassroom

---
+++ proyecto002/src/proyecto002/Proyecto002.java
@@ -0,0 +1,12 @@
+
+package proyecto002;
+
+ 
+public class Proyecto002 {
+
+    
+    public static void main(String[] args) {
+       System.out.println("Ejemplo dos básico de Java Versionado a través de Git Manejado desde GitHub Además de usar GitHubClassroom");
+    }
+    
+}
